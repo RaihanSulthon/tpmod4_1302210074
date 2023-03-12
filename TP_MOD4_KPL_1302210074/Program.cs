4e6363452d3895330c86974764ac8faf265d43f2
RaihanSulthon <raihansy@student.telkomuniversity.ac.id>
2023-03-12 12:24:57 +0700

    menambahkan implementasi dengan table-driven

--- TP_MOD4_KPL_1302210074/Program.cs
+++ TP_MOD4_KPL_1302210074/Program.cs
@@ -1,2 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace TP_MOD4_KPL
+{
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            KodePos table_kode_pos = new KodePos();
+            Console.WriteLine("Kode pos Batununggal");
+            table_kode_pos.getkodepos("Batunuggal");
+            Console.WriteLine("Kode pos semua Daerah");
+            table_kode_pos.getallkodepos();
+        }
+    }
+    class KodePos
+    {
+        Dictionary<string, string> dic = new Dictionary<string, string>(){
+            {"Batununggal", "40266"},
+            {"Kujangsari", "40287"},
+            {"Mengger", "40267"},
+            {"Wates", "40256"},
+            {"Cijaura", "40287"},
+            {"Jatisari", "40286"},
+            {"Margasari", "40286"},
+            {"Sekejati", "40286"},
+            {"Kebonwaru", "40272"},
+            {"Maleer", "40274"},
+            {"Samoja", "40273"},
+        };
+
+        public void getkodepos(string kode)
+        {
+            if(dic.ContainsKey(kode))
+            {
+                Console.WriteLine(kode + " : " + dic[kode]);
+            }
+            else
+            {
+                Console.WriteLine(kode + "Kode tidak terdaftar");
+            }
+        }
+
+        public void getallkodepos()
+        {
+            foreach(KeyValuePair<string, string> i in dic)
+            {
+                Console.WriteLine(i.Key + " : " + i.Value);
+            }
+        }
+    }
+
+
+}
